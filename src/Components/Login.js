79e20c12641afb4228b1f40f7662468fc7f0c50c
jli0423 <justin.li0423@gmail.com>
2018-09-12 17:04:04 -0400

    progress

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -15,10 +15,17 @@ const Login = () => (
         type="password"
       />
       <Submit>
-        Login
+        login
       </Submit>
     </Form>
-
+    <RememberMe>
+      <Checkbox
+        type="checkbox"
+      />
+      <Label>
+          remember me
+      </Label>
+    </RememberMe>
   </Wrapper>
 );
 
@@ -38,7 +45,7 @@ const Form = styled.form`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  box-shadow: 0 1px 20px rgba(0,0,0, .2);
+  box-shadow: 0 1px 15px rgba(0,0,0, .25);
   width: 30em;
   height: 15em;
 `;
@@ -50,10 +57,10 @@ const Input = styled.input`
   margin: 0;
   border: none;
   text-align: center;
-  font-size: 2em;
+  font-size: 1.6em;
   color: ${Colors.DarkPurple};
   border: 1px solid ${Colors.LightPurple};
-  font-weight: 700;
+  font-weight: 600;
 
   &::placeholder {
     color: ${Colors.LightPurpleDark};
@@ -68,7 +75,48 @@ const Submit = styled.button`
   border: none;
   background-color: ${Colors.DarkPurpleDark};
   color: ${Colors.White};
-  font-weight: 700;
+  font-weight: 600;
+  font-size: 1.6em;
+`;
+
+const RememberMe = styled.div`
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Checkbox = styled.input`
+  margin-right: 3em;
+  appearance: none;
+  outline: none;
+
+  &:checked::after {
+    background-color: ${Colors.DarkPurpleDark};
+    border: 3px solid ${Colors.White};
+    border-radius: 3px;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 1.5em;
+    height: 1.5em;
+    margin-top: -.5em;
+    border-radius: 3px;
+    box-shadow: 0 0 0 1px ${Colors.DarkPurpleDark};
+    border: 3px solid ${Colors.LightPurpleLight};
+  }
+`;
+
+const Label = styled.span`
+  font-weight: 600;
+  font-size: 1.5em;
+  color: ${Colors.DarkPurpleDark};
 `;
 
 export default Login;
